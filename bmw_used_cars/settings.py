7b2d5ab73ba024347059f19713beaa389b77da11
Heorhii Nadiradze <swain.volt@gmail.com>
2026-03-09 10:50:59 +0200

    turn on ITEM_PIPELINES in settings.py

--- bmw_used_cars/settings.py
+++ bmw_used_cars/settings.py
@@ -58,9 +58,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "bmw_used_cars.pipelines.BmwUsedCarsPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "bmw_used_cars.pipelines.BmwUsedCarsPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
