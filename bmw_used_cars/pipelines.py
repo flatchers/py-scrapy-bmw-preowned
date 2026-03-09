57f58a227cbebfa5d06feb09a116f5634944ebf8
Heorhii Nadiradze <swain.volt@gmail.com>
2026-03-09 10:50:14 +0200

    add SQLite database pipeline

--- bmw_used_cars/pipelines.py
+++ bmw_used_cars/pipelines.py
@@ -2,12 +2,53 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class BmwUsedCarsPipeline:
-    def process_item(self, item, spider):
+    def open_spider(self):
+        self.conn = sqlite3.connect("bmw_cars.db")
+        self.cur = self.conn.cursor()
+
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS cars(
+            model TEXT,
+            name TEXT,
+            mileage INTEGER,
+            registered TEXT,
+            engine TEXT,
+            range INTEGER,
+            exterior TEXT,
+            fuel TEXT,
+            transmission TEXT,
+            registration TEXT,
+            upholstery TEXT
+        )
+        """)
+
+    def process_item(self, item):
+        self.cur.execute("""
+        INSERT INTO cars VALUES (?,?,?,?,?,?,?,?,?,?,?)
+        """, (
+            item["model"],
+            item["name"],
+            item["mileage"],
+            item["registered"],
+            item["engine"],
+            item["range"],
+            item["exterior"],
+            item["fuel"],
+            item["transmission"],
+            item["registration"],
+            item["upholstery"],
+        ))
+
+        self.conn.commit()
         return item
+
+    def close_spider(self):
+        self.conn.close()
+
